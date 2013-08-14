97c33473371b7bcb8f31f9cd1fedfc543b723424
Michael Williamson <mike@zwobble.org>
2013-08-14 12:00:56 +0100

    Expose and lightly test a distinct game object

--- tests/minesweeper.tests.js
+++ tests/minesweeper.tests.js
@@ -11,6 +11,54 @@ exports["game is initially rendered as all unexplored"] = function(test) {
     test.done();
 };
 
+exports["external game state is initially all unexplored"] = function(test) {
+    var empty = minesweeper.states.empty;
+    var game = minesweeper.createGame([
+        [empty, empty],
+        [empty, empty]
+    ]);
+    test.equal(empty, game.externalState(0, 0));
+    test.equal(empty, game.externalState(0, 1));
+    test.equal(empty, game.externalState(1, 0));
+    test.equal(empty, game.externalState(1, 1));
+    test.done();
+};
+
+exports["external game state for explored square is zero if no mines nearby"] = function(test) {
+    var empty = minesweeper.states.empty;
+    var game = minesweeper.createGame([
+        [empty, empty],
+        [empty, empty]
+    ]);
+    game.explore(0, 0);
+    test.equal(0, game.externalState(0, 0));
+    test.done();
+};
+
+exports["external game state for explored square is number of nearby mines"] = function(test) {
+    var empty = minesweeper.states.empty;
+    var mine = minesweeper.states.mine;
+    var game = minesweeper.createGame([
+        [empty, mine],
+        [mine, mine]
+    ]);
+    game.explore(0, 0);
+    test.equal(3, game.externalState(0, 0));
+    test.done();
+};
+
+exports["external game state for explored square mine if there is a mine at that square"] = function(test) {
+    var empty = minesweeper.states.empty;
+    var mine = minesweeper.states.mine;
+    var game = minesweeper.createGame([
+        [empty, mine],
+        [mine, mine]
+    ]);
+    game.explore(0, 1);
+    test.equal(mine, game.externalState(0, 1));
+    test.done();
+};
+
 
 function fakeIo() {
     var output = [];
